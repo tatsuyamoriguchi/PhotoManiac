f7f6e38cf99ec673a70a6246c3f5143947558fee
Brian Moriguchi <brian@beckos.com>
2022-03-31 11:23:30 -0700

    Initial Commit

---
+++ PhotoManiac/PhotoManiacApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoManiacApp.swift
+//  PhotoManiac
+//
+//  Created by Tatsuya Moriguchi on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoManiacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
